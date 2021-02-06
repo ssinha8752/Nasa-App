2fe2f7681c0fa5888111e8d2ba0a208e1e7bc655
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-02-06 19:21:40 +0530

    [Shubham][WUN-8] Adding route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route} from 'react-router-dom'
 import Header from './Components/Header'
 import Landing from './Components/Landing'
 
 function App() {
   return (
+  <BrowserRouter>
     <div>
-    <Landing/>
+      <Route component={Header} path="/" exact/>
+      <Route component={Landing} path="/login" exact/>
     </div>
-  );
+  </BrowserRouter>
+    );
 }
 
 export default App;
