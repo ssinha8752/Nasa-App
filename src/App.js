a72bdc11db159315dbe44d03510b4327cabab1fa
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-02-06 18:23:46 +0530

    [Shubham][WUN-7] Initializing the Landing page with react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from './Components/Header'
+import Landing from './Components/Landing'
 
 function App() {
   return (
     <div>
-    <Header/>
+    <Landing/>
     </div>
   );
 }
